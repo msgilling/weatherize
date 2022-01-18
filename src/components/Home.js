5ddf7bdcf9e98422b441bed5c92ed05fe5a6c555
Mariah Gilling <mxrlling@gmail.com>
2022-01-18 15:18:15 +0000

    get request response working in console

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 const Home = () => {
+  // const [formData, getFormData] = useState('')
+  // const [formSubmit, getSubmit] = useState('')
+
+  useEffect(() => {
+    const getWeather = async () => {
+      const response = await axios.get("http://api.weatherapi.com/v1/forecast.json?key=62945e3bf23742b1955150259221701&q=northampton&days=1&aqi=no&alerts=no")
+      console.log('response ->', response)
+    }
+    getWeather()
+  }, [])
 
   return(
 
-    <h1>weather app hello</h1>
+    <div className="main-section">
+      
+
+
+    </div>
   )
 }
 
