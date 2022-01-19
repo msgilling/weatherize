352a2a4248e97e5dc82ea7984c5c044fcf64d586
Mariah Gilling <mxrlling@gmail.com>
2022-01-19 17:05:52 +0000

    get weather, location and temperature showing

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,18 +5,25 @@ const Home = () => {
   const [formData, setFormData] = useState('')
   const [formSubmit, setFormSubmit] = useState('')
   const [weather, setWeather] = useState('')
+  const [error, setError] = useState(false)
 
 
   useEffect(() => {
     const getWeather = async () => {
-      const { data } = await axios.get("http://api.weatherapi.com/v1/forecast.json?key=62945e3bf23742b1955150259221701&q={formSubmit}&days=1&aqi=no&alerts=no")
-      console.log('response ->', data)
-      setWeather(data)
+      try {
+        const { data } = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=62945e3bf23742b1955150259221701&q=${formSubmit}&days=1&aqi=no&alerts=no`)
+        console.log('response ->', data.location)
+        console.log('response ->', data.current.condition.text)
+        setWeather(data)
+      } catch (err) {
+        setError(true)
+      }
     }
     getWeather()
   }, [formSubmit])
 
-  console.log('city selected ->', weather)
+  // console.log('city selected ->', weather.current.condition.text)
+  console.log('error', error)
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -49,11 +56,20 @@ const Home = () => {
           />
           <button
             className="submit-button"
-            
           >
             Submit
           </button>
         </form>
+        <div>
+          <div>
+            <p>{weather.location.name}</p>
+            <p>{weather.current.condition.text}</p>
+            <img src={weather.current.condition.icon} alt="weather-icon"/>
+            <p>{weather.current.temp_c}°</p>
+            
+
+          </div>
+        </div>
       </div>
     </div>
   )
