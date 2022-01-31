838ee6773374c68fc691c546641ac2a960a7faaa
Mariah Gilling <mxrlling@gmail.com>
2022-01-31 11:47:00 +0000

    weather bg

---
+++ src/components/WeatherBackground.js
@@ -0,0 +1,45 @@
+// import React from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+// import clearDay from '../assets/CLEAR.PNG'
+// import cloudyDay from '../assets/CLOUDY.PNG'
+// import hazyDay from '../assets/CLOUDY.PNG'
+import partCloudyDay from '../assets/PARTCLOUDY.PNG'
+// import rainingDay from '../assets/RAINY.PNG'
+
+
+const WeatherBackground = () => {
+  const [weather, setWeather] = useState('')
+  
+
+  useEffect(() => {
+    const getWeather = async () => {
+      try {
+        const { data } = await axios.get('http://api.weatherapi.com/v1/forecast.json?key=62945e3bf23742b1955150259221701&q=London&days=1&aqi=yes&alerts=no')
+        // console.log('response ->', response)
+        setWeather(data)
+      } catch (err) {
+        console.log('err', err)
+      }
+    }
+    getWeather()
+  }, [])
+
+
+  console.log('weather', weather.current)
+
+
+
+  return (
+    <div className="currentweather-bg"
+      
+    />
+      // {/* <img src={partCloudyDay} alt="logo" className="weather-bg" /> */}
+    // </div>
+      
+    
+
+  )
+}
+
+export default WeatherBackground
